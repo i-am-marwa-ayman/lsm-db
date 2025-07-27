1ceb29e65e088a0dcd98abb038a051677e43e08f
marwa <i.am.marwa.ayman@gmail.com>
2025-07-27 11:51:24 +0300

    use sstable in engine

--- engine/enging.go
+++ engine/enging.go
@@ -1,28 +1,67 @@
 package engine
 
-import "mini-levelDB/memtable"
+import (
+	"fmt"
+	"log"
+	"mini-levelDB/memtable"
+	"mini-levelDB/sstable"
+)
 
 type Engine struct {
-	Memtable  *memtable.MemTable
-	immutable []*memtable.MemTable
+	memtable       *memtable.MemTable
+	sstableManager sstable.SsManager
 }
 
 func NewEngine() *Engine {
 	return &Engine{
-		Memtable:  memtable.NewMemtable(),
-		immutable: make([]*memtable.MemTable, 0),
+		memtable:       memtable.NewMemtable(),
+		sstableManager: *sstable.NewSsManager(),
 	}
 }
 
-func (DB *Engine) Get(key string) string { // i think i will make the memtable take a []byte
-	val := DB.Memtable.Get(key)
-	return val.Value
+func (db *Engine) Get(key string) (string, error) { // i think i will make the memtable take a []byte
+	val, err := db.memtable.Get(key)
+	if err != nil {
+		if err == fmt.Errorf("val is deleted") {
+			return "", fmt.Errorf("no such a key")
+		}
+		val, err := db.sstableManager.Get(key)
+		if err != nil {
+			return "", fmt.Errorf("no such a key")
+		} else {
+			return val, nil
+		}
+	} else {
+		return val, err
+	}
 }
 
-func (DB *Engine) Set(key string, val string) {
-	DB.Memtable.Set(key, val)
+func (db *Engine) Set(key string, val string) {
+	db.memtable.Set(key, val)
+	log.Printf("key %s is inserted\n", key)
+	if db.memtable.IsFull() {
+		log.Println("full table")
+		log.Println("loading to disk...")
+		err := db.sstableManager.AddSstable(db.memtable.GetAll())
+		if err != nil {
+			log.Println("somthing worg happend!")
+			log.Println(err)
+		}
+		db.memtable = memtable.NewMemtable()
+	}
 }
 
-func (DB *Engine) Delete(key string) {
-	DB.Memtable.Delete(key)
+func (db *Engine) Delete(key string) {
+	db.memtable.Delete(key)
+	log.Printf("key %s is deleted\n", key)
+	if db.memtable.IsFull() {
+		log.Println("full table")
+		log.Println("loading to disk...")
+		err := db.sstableManager.AddSstable(db.memtable.GetAll())
+		if err != nil {
+			log.Println("somthing worg happend!")
+			log.Println(err)
+		}
+		db.memtable = memtable.NewMemtable()
+	}
 }
